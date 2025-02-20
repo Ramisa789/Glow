7ff08e8069790155ec0dc3b0270ad41e346c60ea
sofia tapias <sofia.tapiasmontana@ucalgary.ca>
2025-02-19 22:53:21 -0700

    Fixed spelling mistakes, removed exec and aded <Router>

--- temp/frontend/src/App.js
+++ temp/frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './Pages/LanginPage';
+import LandingPage from './Pages/LandingPage';
 import Login from './Pages/Login';
 import SignUp from './Pages/SignUp';
 import SkinCareGenerator from './Pages/SkinCareGenerator';
@@ -10,11 +10,13 @@ function App(){
   return (
     <div>
       <Router>
-        <Route exac path = '//' element={<LandingPage/>}/>
-        <Route exac path = '/login' element={<Login/>}/>
-        <Route exac path = '/SignUp' element={<SignUp/>}/>
-        <Route exac path = '/SkinCareGenerator' element={<SkinCareGenerator/>}/>
-        <Route exac path = '/UserProfile' element={<UserProfile/>}/>
+        <Routes>
+          <Route path = '//' element={<LandingPage/>}/>
+          <Route path = '/login' element={<Login/>}/>
+          <Route path = '/SignUp' element={<SignUp/>}/>
+          <Route path = '/SkinCareGenerator' element={<SkinCareGenerator/>}/>
+          <Route path = '/UserProfile' element={<UserProfile/>}/>
+        </Routes>
       </Router>
     </div>
   );
