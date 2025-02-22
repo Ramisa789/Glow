efbac5f41fdd2ad6895f957a735e823290576e7f
hannagracec <hannacho847@gmail.com>
2025-02-21 20:08:28 -0700

    added comments to settings

--- Glow/backend/config/settings.py
+++ Glow/backend/config/settings.py
@@ -83,10 +83,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DB_NAME'),  
-        'USER': os.getenv('DB_USER', 'postgres'),
+        'USER': os.getenv('DB_USER', 'postgres'), # default to postgres user if not provided
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'), # default to 127.0.0.1 if not provided
+        'PORT': os.getenv('DB_PORT', '5432'), # default to port 5432 if not provided
     }
 }
 
