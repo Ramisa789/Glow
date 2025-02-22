f949ad70d36a14db1dc5768f20dcdd25d11f856f
hannagracec <hannacho847@gmail.com>
2025-02-21 18:47:13 -0700

    renamed files for organization

--- Glow/backend/config/settings.py
+++ Glow/backend/config/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'test1', 
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': '123',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
