af7a30d051841511e928caaa046341a89b133e83
Oliver-Molina <98718412+Oliver-Molina@users.noreply.github.com>
2025-02-27 12:16:32 -0700

    SCRUM-44: Creating Database Table Models (#5)
    Adding v1 database tables for Glow App.

--- Glow/backend/api/models.py
+++ Glow/backend/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name + " - " + self.price 
+    
+class Routine(models.Model):
+    products = models.ManyToManyField(Product, through='RoutineProduct')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    time_of_day = models.CharField(max_length=5, choices=[('day', 'Day'), ('night', 'Night')])
+
+class RoutineProduct(models.Model):
+    routine = models.ForeignKey(Routine, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
