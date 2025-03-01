0e5a61c2612164fa6088d0f77d24b6cffc6e34e5
sofia tapias <sofia.tapiasmontana@ucalgary.ca>
2025-02-28 19:48:22 -0700

    base strcuture of the saved routines page

---
+++ Glow/frontend/src/Pages/savedRoutines.js
@@ -0,0 +1,14 @@
+import "./savedRoutines.css";
+import React from 'react';
+import Header from './Components/header';
+import { Link } from 'react-router-dom';
+
+export default function savedRoutines() {
+    return(
+       <div>
+       <Header />
+       
+        
+       </div>
+    );
+}
