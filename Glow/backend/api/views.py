444f3ea00347919fb1e3aea535ab08974a788936
hannagracec <hannacho847@gmail.com>
2025-03-01 15:24:48 -0700

    update gemini model

--- Glow/backend/api/views.py
+++ Glow/backend/api/views.py
@@ -60,7 +60,7 @@ genai.configure(api_key=settings.GEMINI_API_KEY)
 def generate_response(request):
     print("debugging print statment for this url")
     user_input = request.data.get("prompt", "")
-    model = genai.GenerativeModel("gemini")
+    model = genai.GenerativeModel("gemini-2.0-flash")
     response = model.generate_content(user_input)
     
     return Response({"response": response.text})
