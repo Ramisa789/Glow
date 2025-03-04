c3193b16df930f9a48a53975ef35b91064793f03
Oliver-Molina <98718412+Oliver-Molina@users.noreply.github.com>
2025-03-03 17:57:08 -0700

    Updating models to include instructions

--- Glow/backend/api/models.py
+++ Glow/backend/api/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 # Create your models here.
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
@@ -18,3 +18,4 @@ class Routine(models.Model):
 class RoutineProduct(models.Model):
     routine = models.ForeignKey(Routine, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    instructions = models.CharField(max_length=400, null=False, default="N/A")
